df90e2b9d3507c0ab3246e37666cb1fe5f8dd62e
codedit334 <oussama7elabdioui@gmail.com>
2023-06-07 10:26:13 +0100

    Change symbol from user to author

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  has_one_attached :photo
+  # has_one_attached :photo
   has_many :likes, foreign_key: 'author_id' # specify the custom foreign key name
+  has_many :posts, foreign_key: 'author_id' # specify the custom foreign key name
+  has_many :comments, foreign_key: 'author_id' # specify the custom foreign key name
 end
