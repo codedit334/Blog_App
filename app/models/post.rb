1ddf661b5d3842cc5d3e9843b2c474929a5214a1
codedit334 <oussama7elabdioui@gmail.com>
2023-06-07 15:52:57 +0100

    Add validations for User

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
 
+  
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
