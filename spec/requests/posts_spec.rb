a4e238436b1e774d62456b76f7439d30e3248c0d
codedit334 <oussama7elabdioui@gmail.com>
2023-06-13 13:27:35 +0100

    Run rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,33 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     let(:user) { FactoryBot.create(:user) }
 
     before do
       FactoryBot.create_list(:post, 3, author: user)
     end
 
-    it "returns http success and renders the index template" do
+    it 'returns http success and renders the index template' do
       get :index, params: { user_id: user.id }
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET #show" do
+  describe 'GET #show' do
     let(:user) { FactoryBot.create(:user) }
     let(:post) { FactoryBot.create(:post, author: user) }
 
     before do
-      FactoryBot.create_list(:comment, 3, post: post)
+      FactoryBot.create_list(:comment, 3, post:)
     end
 
-    it "returns http success and renders the show template" do
+    it 'returns http success and renders the show template' do
       get :show, params: { user_id: user.id, id: post.id }
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
     end
   end
 end
-
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET #index" do
-    it "returns http success and renders the index template" do
+  describe 'GET #index' do
+    it 'returns http success and renders the index template' do
       get :index
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET #show" do
+  describe 'GET #show' do
     let(:user) { FactoryBot.create(:user) }
 
-    it "returns http success and renders the show template" do
+    it 'returns http success and renders the show template' do
       get :show, params: { id: user.id }
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
