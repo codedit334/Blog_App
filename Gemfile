0f8b94a900ba61fc452cda28f407a362021671f8
codedit334 <oussama7elabdioui@gmail.com>
2023-06-13 13:20:57 +0100

    Add User controller test

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'rubocop'
   gem 'shoulda-matchers'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,28 @@
+# require 'rails_helper'
+
+# RSpec.describe 'Users', type: :request do
+#   describe 'GET /index' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+# end
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UsersController, type: :controller do
+  describe "GET #index" do
+    it "returns http success and renders the index template" do
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #show" do
+    let(:user) { FactoryBot.create(:user) }
+
+    it "returns http success and renders the show template" do
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
   end
 end
