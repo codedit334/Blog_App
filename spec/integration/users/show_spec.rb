1ddcf1ec0ff60da5fb68e75bd2d90b21d15ef3ca
codedit334 <oussama7elabdioui@gmail.com>
2023-06-19 17:17:30 +0100

    Add user show tests

--- spec/integration/users/show_spec.rb
+++ spec/integration/users/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'User show page' do
   scenario 'displays user profile picture, name, number of posts, bio, and first 3 posts' do
     user = FactoryBot.create(:user)
+    post4 = FactoryBot.create(:post, author: user, text: 'First post')
     posts = FactoryBot.create_list(:post, 5, author: user)
 
     visit user_path(user)
@@ -12,11 +13,10 @@ RSpec.feature 'User show page' do
     expect(page).to have_content("Number of posts: #{user.posts.count}")
     expect(page).to have_content(user.bio)
 
-    expect(page).to have_content(posts[0].title)
-    expect(page).to have_content(posts[1].title)
-    expect(page).to have_content(posts[2].title)
-    expect(page).not_to have_content(posts[3].title)
-    expect(page).not_to have_content(posts[4].title)
+    expect(page).to have_content(posts[0].text)
+    expect(page).to have_content(posts[1].text)
+    expect(page).to have_content(posts[2].text)
+    expect(page).not_to have_content(post4.text)
 
     expect(page).to have_link('See all posts', href: user_posts_path(user))
   end
@@ -26,10 +26,9 @@ RSpec.feature 'User show page' do
     post = FactoryBot.create(:post, author: user)
 
     visit user_path(user)
-    click_link post.title
+    click_link post.text
 
     expect(current_path).to eq(user_post_path(user, post))
-    expect(page).to have_content(post.title)
     expect(page).to have_content(post.text)
   end
 
@@ -40,6 +39,6 @@ RSpec.feature 'User show page' do
     click_link 'See all posts'
 
     expect(current_path).to eq(user_posts_path(user))
-    expect(page).to have_content("#{user.name}'s posts")
+    expect(page).to have_content("Name: #{user.name}")
   end
 end
