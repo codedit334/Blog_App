329bf044a14ebe8d034037107a7f6e7fec8e63ba
codedit334 <oussama7elabdioui@gmail.com>
2023-06-21 18:44:16 +0100

    Add CASCADE Constraints

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,10 +9,15 @@ class Ability
       else
         can :read, :all
         can :create, [Post, Comment]
-        can :update, [Post, Comment], author: user.id
-        can :destroy, Post, author: user.id
-        can :destroy, Comment, author: user.id
+        can :update, [Post, Comment], author_id: user.id
+        can :destroy, Post, author_id: user.id
+        can :destroy, Comment, author_id: user.id
       end
+
+    #   puts "User #{user.id} permissions:"
+    #   puts "can destroy [@user, @post]: #{can? :destroy, [@user, @post]}"
+      puts "User #{user.id} permissions:"
+      puts "can destroy Post: #{can? :destroy, Post}"
     end
-  end
+end
   
